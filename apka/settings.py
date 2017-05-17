833f5d1ef2ab06fabf44ae45c5560abc15a93f69
MrChudy923 <fikama290@vp.pl>
2017-05-17 16:21:36 -0400

    v4

--- apka/settings.py
+++ apka/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4h@d4x7v*+%#y^-5g+4g708@=ipz)vcx9uxk1oe)u^o3mpj6ef'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,5 +119,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
